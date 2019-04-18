40496cd0f98286cc10cdda1598fb036d6cbb0ab6
Sol Boucher <sboucher@cmu.edu>
2019-04-18 00:13:39 -0400

    interpose/dlinterp.c: Increase the difficulty by also interposing mprotect() and sysconf()

--- interpose/dlinterp.c
+++ interpose/dlinterp.c
@@ -1,5 +1,7 @@
+#include <sys/mman.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <unistd.h>
 
 void *dlopen(const char *filename, int flags) {
 	puts("dlopen() from libdlinterp");
@@ -9,4 +11,14 @@ void *dlopen(const char *filename, int flags) {
 void *dlsym(void *handle, const char *symbol) {
 	puts("dlsym() from libdlinterp");
 	return dlsym(handle, symbol);
+}
+
+int mprotect(void *addr, size_t len, int prot) {
+	puts("mprotect() from libdlinterp");
+	return mprotect(addr, len, prot);
+}
+
+long sysconf(int name) {
+	puts("sysconf() from libdlinterp");
+	return sysconf(name);
 }
