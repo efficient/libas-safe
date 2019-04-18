abe52ab33d7f6c1ff73eb3cd29ad13a47940bf54
Sol Boucher <sboucher@cmu.edu>
2019-04-18 00:57:46 -0400

    interpose/dlinterp.c: Increase the difficulty by interposing strcmp()

--- interpose/dlinterp.c
+++ interpose/dlinterp.c
@@ -1,6 +1,7 @@
 #include <sys/mman.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 void *dlopen(const char *filename, int flags) {
@@ -16,6 +17,11 @@ void *dlsym(void *handle, const char *symbol) {
 int mprotect(void *addr, size_t len, int prot) {
 	puts("mprotect() from libdlinterp");
 	return mprotect(addr, len, prot);
+}
+
+int strcmp(const char *s1, const char *s2) {
+	puts("strcmp() from libdlinterp");
+	return strcmp(s1, s2);
 }
 
 long sysconf(int name) {
