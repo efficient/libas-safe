ebf8b9f000adadd1a72f8ac06eed033e79154480
Sol Boucher <sboucher@cmu.edu>
2020-07-27 15:04:52 -0400

    tls/tcb.c: Fix operation with -fpic

--- tls/tcb.c
+++ tls/tcb.c
@@ -7,35 +7,36 @@
 
 static thread_local bool ean;
 
-int arch_prctl(int, const void **);
-void *_dl_allocate_tls(void *);
-void _dl_deallocate_tls(void *, bool);
+int arch_prctl(int, const void ***);
+void **_dl_allocate_tls(void **);
+void _dl_deallocate_tls(void **, bool);
 
 static void *timed(void) {
-	void *lpfcb;
-	const void *tcb;
+	void **lpfcb;
+	const void **tcb;
 	lpfcb = _dl_allocate_tls(NULL);
+	*lpfcb = lpfcb;
 	arch_prctl(ARCH_GET_FS, &tcb);
-	arch_prctl(ARCH_SET_FS, (const void **) lpfcb);
+	arch_prctl(ARCH_SET_FS, (const void ***) lpfcb);
 
 	assert(!ean);
 	ean = true;
 	assert(ean);
 
-	arch_prctl(ARCH_SET_FS, (const void **) tcb);
+	arch_prctl(ARCH_SET_FS, (const void ***) tcb);
 	return lpfcb;
 }
 
 static void *threaded(void *lpfcb) {
 	assert(!ean);
 
-	const void *tcb;
+	const void **tcb;
 	arch_prctl(ARCH_GET_FS, &tcb);
-	arch_prctl(ARCH_SET_FS, (const void **) lpfcb);
+	arch_prctl(ARCH_SET_FS, (const void ***) lpfcb);
 
 	assert(ean);
 
-	arch_prctl(ARCH_SET_FS, (const void **) tcb);
+	arch_prctl(ARCH_SET_FS, (const void ***) tcb);
 	_dl_deallocate_tls(lpfcb, true);
 	return NULL;
 }
