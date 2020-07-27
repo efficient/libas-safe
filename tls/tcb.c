4e19321ebcee895f104af556671ba71276510d82
Sol Boucher <sboucher@cmu.edu>
2020-07-27 12:00:10 -0400

    tls: Proof-of-concept custom TCB program that only works when compiled without -fpic

---
+++ tls/tcb.c
@@ -0,0 +1,49 @@
+#include <asm/prctl.h>
+#include <assert.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <threads.h>
+
+static thread_local bool ean;
+
+int arch_prctl(int, const void **);
+void *_dl_allocate_tls(void *);
+void _dl_deallocate_tls(void *, bool);
+
+static void *timed(void) {
+	void *lpfcb;
+	const void *tcb;
+	lpfcb = _dl_allocate_tls(NULL);
+	arch_prctl(ARCH_GET_FS, &tcb);
+	arch_prctl(ARCH_SET_FS, (const void **) lpfcb);
+
+	assert(!ean);
+	ean = true;
+	assert(ean);
+
+	arch_prctl(ARCH_SET_FS, (const void **) tcb);
+	return lpfcb;
+}
+
+static void *threaded(void *lpfcb) {
+	assert(!ean);
+
+	const void *tcb;
+	arch_prctl(ARCH_GET_FS, &tcb);
+	arch_prctl(ARCH_SET_FS, (const void **) lpfcb);
+
+	assert(ean);
+
+	arch_prctl(ARCH_SET_FS, (const void **) tcb);
+	_dl_deallocate_tls(lpfcb, true);
+	return NULL;
+}
+
+int main(void) {
+	void *state = timed();
+	pthread_t nation;
+	pthread_create(&nation, NULL, threaded, state);
+	pthread_join(nation, NULL);
+	return 0;
+}
