4772b727a4e7fa46aa196ea2965698884685eada
Sol Boucher <sboucher@cmu.edu>
2020-08-26 03:50:40 -0400

    tls/perf.c: Benchmark _dl_allocate_tls() instead

--- tls/perf.c
+++ tls/perf.c
@@ -1,9 +1,9 @@
-#include "libinger.h"
-
 #include <stdio.h>
 #include <time.h>
 
-#define ITERS 10000
+#define ITERS 10000000
+
+void *_dl_allocate_tls(void *);
 
 static unsigned long nsnow(void) {
 	struct timespec tv;
@@ -11,14 +11,10 @@ static unsigned long nsnow(void) {
 	return tv.tv_sec * 1000000000 + tv.tv_nsec;
 }
 
-static void nop(void *nope) {
-	(void) nope;
-}
-
 int main(void) {
 	unsigned long nsthen = nsnow();
 	for(int iter = 0; iter < ITERS; ++iter)
-		launch(nop, -1, NULL);
+		_dl_allocate_tls(NULL);
 
 	unsigned long nswhen = (nsnow() - nsthen) / ITERS;
 	printf("%ld.%03ld μs\n", nswhen / 1000, nswhen % 1000);
