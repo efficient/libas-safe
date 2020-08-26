4f68281391559cdb58b7b77faa15d4d74e4f1ad6
Sol Boucher <sboucher@cmu.edu>
2020-08-26 03:40:33 -0400

    tls/perf.c: Attempt to replicate libinger's 'launch' benchmark's use of multiple libsets
    but it is unstable.

--- tls/perf.c
+++ tls/perf.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 #define ITERS 511
+#define PERSIST
 
 static unsigned long nsnow(void) {
 	struct timespec tv;
@@ -27,6 +28,18 @@ int main(void) {
 
 	unsigned long nswhen = (nsnow() - nsthen) / ITERS;
 	printf("%ld.%03ld μs\n", nswhen / 1000, nswhen % 1000);
+
+#ifdef PERSIST
+	linger_t *things = malloc(ITERS * sizeof things);
+	nsthen = nsnow();
+	for(int iter = 0; iter < ITERS; ++iter)
+		things[iter] = launch(bop, -1, NULL);
+	nswhen = (nsnow() - nsthen) / ITERS;
+	for(int iter = 0; iter < ITERS; ++iter)
+		resume(things + iter, -1);
+	free(things);
+	printf("%ld.%03ld μs\n", nswhen / 1000, nswhen % 1000);
+#endif
 
 	return 0;
 }
