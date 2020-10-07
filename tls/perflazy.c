cd7270f22c8326de3d00edec44899c23a91b1925
Sol Boucher <sboucher@cmu.edu>
2020-10-07 17:15:42 -0400

    tls: Make perflazy proof-of-concept example skip TLS allocation, which will work for non-LPF TCBs
    Comparing
     $ ./perflazy libinger/target/release/libinger.so
    against
     $ ./perflazy libinger/target/release/libinger.so yes
    reveals orders of magnitude performance difference when run against libinger 7b146a6.

--- tls/perflazy.c
+++ tls/perflazy.c
@@ -1,5 +1,9 @@
+#include <dlfcn.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 #define ITERS 1000000
@@ -7,13 +11,36 @@
 void *_dl_allocate_tls(void *);
 void _dl_deallocate_tls(void *, bool);
 
+extern uint8_t _rtld_global[];
+
+static size_t *dtv_max_idx(void) {
+	return (size_t *) (_rtld_global + 83264);
+}
+
 static unsigned long nsnow(void) {
 	struct timespec tv;
 	clock_gettime(CLOCK_REALTIME, &tv);
 	return tv.tv_sec * 1000000000 + tv.tv_nsec;
 }
 
-int main(void) {
+int main(int argc, char **argv) {
+	if(argc > 1) {
+		size_t *tls_max_dtv_idx = dtv_max_idx();
+		size_t snapshot = *tls_max_dtv_idx;
+		printf("snap: %lu\n", snapshot);
+		for(int i = 0; i < 511; ++i)
+			if(!dlmopen(LM_ID_NEWLM, argv[1], RTLD_NOW)) {
+				fprintf(stderr, "%s\n", dlerror());
+				return 2;
+			}
+		printf("done: %lu\n", *tls_max_dtv_idx);
+		if(argc > 2 && !strcmp(argv[2], "yes"))
+			*tls_max_dtv_idx = snapshot;
+	} else {
+		printf("USAGE: %s <filename> [yes]\n", argv[0]);
+		return 1;
+	}
+
 	unsigned long nsthen = nsnow();
 	for(int iter = 0; iter < ITERS; ++iter)
 		_dl_deallocate_tls(_dl_allocate_tls(NULL), true);
