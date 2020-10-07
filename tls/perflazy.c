1e97a2615e11fa75826880714c3d0072968403f9
Sol Boucher <sboucher@cmu.edu>
2020-10-07 16:43:17 -0400

    tls: Copy perf program to support experiments into partially deferring TLS initialization

---
+++ tls/perflazy.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+
+#define ITERS 1000000
+
+void *_dl_allocate_tls(void *);
+void _dl_deallocate_tls(void *, bool);
+
+static unsigned long nsnow(void) {
+	struct timespec tv;
+	clock_gettime(CLOCK_REALTIME, &tv);
+	return tv.tv_sec * 1000000000 + tv.tv_nsec;
+}
+
+int main(void) {
+	unsigned long nsthen = nsnow();
+	for(int iter = 0; iter < ITERS; ++iter)
+		_dl_deallocate_tls(_dl_allocate_tls(NULL), true);
+
+	unsigned long nswhen = (nsnow() - nsthen) / ITERS;
+	printf("%ld.%03ld μs\n", nswhen / 1000, nswhen % 1000);
+
+	return 0;
+}
