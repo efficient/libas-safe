159bb611435db3f548b91c4fdd4e21e4fe602b30
Sol Boucher <sboucher@cmu.edu>
2020-10-14 00:45:10 -0400

    tlsblock: Implement libtlsblock library for initializing TCBs using CoW

---
+++ tlsblock/tlsblock.c
@@ -0,0 +1,90 @@
+#include "tlsblock.h"
+
+#include <sys/mman.h>
+#include <assert.h>
+#include <dlfcn.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <threads.h>
+#include <unistd.h>
+
+#define BACKING_FILENAME "/tmp/libtlsblock-XXXXXX"
+
+#define INTERPOSE(ret, fun, ...) \
+	ret fun(__VA_ARGS__) { \
+		static ret (*fun)(__VA_ARGS__) = NULL; \
+		if(!fun) \
+			*(void **) &fun = dlsym(RTLD_NEXT, #fun); \
+
+static int template;
+static size_t template_size;
+static char template_filename[] = BACKING_FILENAME;
+
+static thread_local bool recording;
+static thread_local const uint8_t *recording_addr;
+static thread_local size_t recording_size;
+
+void _dl_deallocate_tls(void *, bool);
+
+INTERPOSE(void *, _dl_allocate_tls, void *existing) //{
+	if(template)
+		return mmap(NULL, template_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, template, 0);
+
+	if(!existing)
+		recording = true;
+	return _dl_allocate_tls(existing);
+}
+
+INTERPOSE(void, _dl_deallocate_tls, void *addr, bool full) //{
+	if(template)
+		munmap(addr, template_size);
+	else
+		_dl_deallocate_tls(addr, full);
+}
+
+INTERPOSE(void *, malloc, size_t arg) //{
+	void *res = malloc(arg);
+	if(recording) {
+		recording_addr = res;
+		recording_size = arg;
+		recording = false;
+	}
+	return res;
+}
+
+bool tlsblock_init(void) {
+	static bool idempotent;
+	if(atomic_exchange(&idempotent, true))
+		return true;
+
+	int fd = mkstemp(template_filename);
+	if(fd <= 0)
+		return false;
+
+	void *handle = _dl_allocate_tls(NULL);
+	assert(handle);
+	assert(recording_addr);
+	assert(recording_size);
+
+	const uint8_t *addr = recording_addr;
+	while(addr != recording_addr + recording_size)
+		addr += write(fd, addr, recording_addr + recording_size - addr);
+
+	_dl_deallocate_tls(handle, true);
+	handle = NULL;
+
+	template_size = recording_size;
+	__asm__("" ::: "memory");
+	template = fd;
+
+	return true;
+}
+
+void tlsblock_cleanup(void) {
+	int fd = atomic_exchange(&template, 0);
+	if(fd) {
+		close(fd);
+		unlink(template_filename);
+	}
+}
---
+++ tlsblock/tlsblock.h
@@ -0,0 +1,9 @@
+#ifndef TLSBLOCK_H_
+#define TLSBLOCK_H_
+
+#include <stdbool.h>
+
+bool tlsblock_init(void);
+void tlsblock_cleanup(void);
+
+#endif
