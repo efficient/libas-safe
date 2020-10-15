4ed3a7f67dd7782742a911ce1e8fc1c72c11f9b8
Sol Boucher <sboucher@cmu.edu>
2020-10-14 22:53:29 -0400

    tlsblock/tlsblock.c: Support copy allocation by default but gate CoW behind an environment variable

--- tlsblock/tlsblock.c
+++ tlsblock/tlsblock.c
@@ -6,10 +6,12 @@
 #include <stdatomic.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <threads.h>
 #include <unistd.h>
 
-#define BACKING_FILENAME "/tmp/libtlsblock-XXXXXX"
+#define COW_CONFIG "LIBTLSB_COW"
+#define COW_FILENAME "/tmp/libtlsblock-XXXXXX"
 
 #define INTERPOSE(ret, fun, ...) \
 	ret fun(__VA_ARGS__) { \
@@ -18,9 +20,10 @@
 			*(void **) &fun = dlsym(RTLD_NEXT, #fun); \
 
 static bool template;
+static void *template_addr;
 static int template_fd;
 static size_t template_size;
-static char template_filename[] = BACKING_FILENAME;
+static char template_filename[] = COW_FILENAME;
 
 static thread_local bool recording;
 static thread_local const uint8_t *recording_addr;
@@ -29,8 +32,14 @@ static thread_local size_t recording_size;
 void _dl_deallocate_tls(void *, bool);
 
 INTERPOSE(void *, _dl_allocate_tls, void *existing) //{
-	if(template)
-		return mmap(NULL, template_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, template_fd, 0);
+	if(template) {
+		if(template_addr) {
+			void *res = malloc(template_size);
+			memcpy(res, template_addr, template_size);
+			return res;
+		} else
+			return mmap(NULL, template_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, template_fd, 0);
+	}
 
 	if(!existing)
 		recording = true;
@@ -39,7 +48,10 @@ INTERPOSE(void *, _dl_allocate_tls, void *existing) //{
 
 INTERPOSE(void, _dl_deallocate_tls, void *addr, bool full) //{
 	if(template)
-		munmap(addr, template_size);
+		if(template_addr)
+			free(addr);
+		else
+			munmap(addr, template_size);
 	else
 		_dl_deallocate_tls(addr, full);
 }
@@ -59,32 +71,44 @@ bool tlsblock_init(void) {
 	if(atomic_exchange(&idempotent, true))
 		return true;
 
-	int fd = mkstemp(template_filename);
-	if(fd < 0)
-		return false;
-
 	void *handle = _dl_allocate_tls(NULL);
 	assert(handle);
 	assert(recording_addr);
 	assert(recording_size);
 
-	const uint8_t *addr = recording_addr;
-	while(addr != recording_addr + recording_size)
-		addr += write(fd, addr, recording_addr + recording_size - addr);
+	if(getenv(COW_CONFIG)) {
+		int fd = mkstemp(template_filename);
+		if(fd < 0)
+			return false;
 
-	_dl_deallocate_tls(handle, true);
-	handle = NULL;
+		const uint8_t *addr = recording_addr;
+		while(addr != recording_addr + recording_size)
+			addr += write(fd, addr, recording_addr + recording_size - addr);
 
-	template_fd = fd;
+		template_fd = fd;
+	} else {
+		void *addr = malloc(recording_size);
+		if(!addr)
+			return false;
+		memcpy(addr, recording_addr, recording_size);
+		template_addr = addr;
+	}
 	template_size = recording_size;
 	__asm__("" ::: "memory");
 	template = true;
 
+	_dl_deallocate_tls(handle, true);
 	return true;
 }
 
 void tlsblock_cleanup(void) {
-	if(atomic_exchange(&template, false)) {
+	if(!atomic_exchange(&template, false))
+		return;
+
+	if(template_addr) {
+		free(template_addr);
+		template_addr = NULL;
+	} else {
 		close(template_fd);
 		template_fd = 0;
 		unlink(template_filename);
