e0fef16a70acb9b842a2975b3d96b70e8b425898
Sol Boucher <sboucher@cmu.edu>
2020-11-06 17:52:52 -0500

    tlsblock: Make thread test program also output and verify offset

--- tlsblock/thread.c
+++ tlsblock/thread.c
@@ -1,13 +1,25 @@
-#include <pthread.h>
+#include "offset.h"
 
-static void *thread(void *ign) {
-	(void) ign;
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+
+static void *thread(void *set) {
+	uintptr_t off = (uintptr_t) set;
+	unsigned *ptr = (unsigned *) (addressof_tcb() - off);
+	assert(*ptr == 0xdeadbeef);
 	return NULL;
 }
 
 int main(void) {
+	static thread_local unsigned sentinel = 0xdeadbeef;
+	size_t off = offsetof_tls(&sentinel);
+	++sentinel;
+
 	pthread_t tid;
-	pthread_create(&tid, NULL, thread, NULL);
+	pthread_create(&tid, NULL, thread, (void *) off);
 	pthread_join(tid, NULL);
+
+	printf("offset: %lu\n", off);
 	return 0;
 }
