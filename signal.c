e0865de3a9974b14f2772dde6e1d3620aacf8907
Sol Boucher <sboucher@cmu.edu>
2022-01-30 18:18:55 -0500

    signal.c: Copy test case from the gotchyas branch
    This seems to have broken sometime between glibc 2.29 and 2.33.  Apparently flockfile() and the
    internal equivalents now take recursive locks, so intra-thread contention no longer deadlocks.

---
+++ signal.c
@@ -0,0 +1,51 @@
+#include <sys/time.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+
+static void handler(int ign) {
+	(void) ign;
+
+	int erryes = errno;
+
+	static char acter = '|';
+	printf("%c\b", acter);
+	fflush(stdout);
+	switch(acter) {
+	case '|':
+		acter = '/';
+		break;
+	case '/':
+		acter = '-';
+		break;
+	case '-':
+		acter = '\\';
+		break;
+	case '\\':
+		acter = '|';
+		break;
+	}
+
+	errno = erryes;
+}
+
+int main(void) {
+	struct sigaction sa = {
+		.sa_handler = handler,
+	};
+	sigaction(SIGALRM, &sa, NULL);
+
+	struct timeval tv = {
+		.tv_usec = 500000,
+	};
+	struct itimerval it = {
+		.it_interval = tv,
+		.it_value = tv,
+	};
+	setitimer(ITIMER_REAL, &it, NULL);
+
+	for(;;)
+		fflush(stdout);
+
+	return 0;
+}
