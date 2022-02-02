c903cb9c2f501149180fa9efe08819213856b631
Sol Boucher <sboucher@cmu.edu>
2022-02-02 08:44:27 -0500

    Revert "Rearchitect test to force a signal to arise within glibc by allocating an artificially small stack"
    This reverts commit 02b66200e02788bab9957ed9e6c4e16be6cdfa93.
    
    In addition to the described build system limitations, this had two shortcomings:
     * The use of SIGSEGV conflicted in some way with global variable interposition, a problem that
       manifested even after tweaking libgotcha to install its own handler with SA_ONSTACK.
     * Having the handler perform necessary fixup meant that it was no longer possible to demonstrate
       deferred mode by adding $LIBGOTCHA_SHAREDLIBC to the environment.

--- signal.c
+++ signal.c
@@ -1,50 +1,51 @@
-#include <sys/mman.h>
+#include <sys/time.h>
 #include <errno.h>
 #include <signal.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
 
-static size_t pagesz;
-static uint8_t *stack;
+static void handler(int ign) {
+	(void) ign;
 
-static int enablepage(void *page) {
-	return mprotect(page, pagesz, PROT_READ | PROT_WRITE);
-}
-
-static void growstack(int signum) {
 	int erryes = errno;
-	(void) signum;
 
-	puts("Growing stack...");
-	enablepage(stack);
+	static char acter = '|';
+	printf("%c\b", acter);
+	fflush(stdout);
+	switch(acter) {
+	case '|':
+		acter = '/';
+		break;
+	case '/':
+		acter = '-';
+		break;
+	case '-':
+		acter = '\\';
+		break;
+	case '\\':
+		acter = '|';
+		break;
+	}
 
 	errno = erryes;
 }
 
 int main(void) {
 	struct sigaction sa = {
-		.sa_handler = growstack,
-		.sa_flags = SA_ONSTACK,
+		.sa_handler = handler,
 	};
-	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGALRM, &sa, NULL);
 
-	stack_t ss = {
-		.ss_sp = (void *) ((uintptr_t) &ss - SIGSTKSZ),
-		.ss_size = SIGSTKSZ,
+	struct timeval tv = {
+		.tv_usec = 500000,
 	};
-	sigaltstack(&ss, NULL);
+	struct itimerval it = {
+		.it_interval = tv,
+		.it_value = tv,
+	};
+	setitimer(ITIMER_REAL, &it, NULL);
 
-	pagesz = sysconf(_SC_PAGE_SIZE);
-	stack = mmap(NULL, pagesz * 2, PROT_NONE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	enablepage(stack + pagesz);
+	for(;;)
+		fflush(stdout);
 
-	puts("Installing custom stack...");
-	__asm__ volatile("mov %0, %%rsp" :: "r" (stack + pagesz + 0x40));
-
-	puts("Reinstalling original stack...");
-	__asm__ volatile("mov %0, %%rsp" :: "r" (&ss));
-
-	munmap(stack, pagesz * 2);
 	return 0;
 }
